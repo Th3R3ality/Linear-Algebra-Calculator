a527ffca4de7f5cd20f6ab6801e59a28e15afebe
Th3R3ality <oliver.tscherning@gmail.com>
2021-08-21 00:05:27 +0200

    regression works

--- Linear Algebra Calculator/calculate.hpp
+++ Linear Algebra Calculator/calculate.hpp
@@ -31,44 +31,65 @@ double constant(double x, double y) {
 	return y / x;
 }
 
-std::vector<double> fillVector(std::vector<double> x) {
+std::vector<double> fillVector(std::vector<double> x, size_t max = 0) {
 	double Temp;
 	while (std::cin >> Temp) {
 		x.push_back(Temp);
+		if (x.size() == max) {
+			break;
+		}
 	}
 	std::cin.clear();
-	for (int i = 0; i < x.size(); i++) {
-		printf("x[%i]\t=\t%f\n", i, x[i]);
-	}
-	std::cin.get();
 	return x;
 }
 
 double mean(std::vector<double> x) {
 	double m = 0;
-	for (int i = 0; i < x.size(); i++) {
+	for (size_t i = 0; i < x.size(); i++) {
 		m += x[i];
 	}
+	m /= x.size();
 	return m;
 }
 
-double pearson(std::vector<double> x, std::vector<double> y) {
-	double r = 0, xM = 0, yM = 0, xyM_sum = 0, xxM2_sum = 0, yyM2_sum = 0;
+std::tuple<double, double, double>regression(std::vector<double> &x, std::vector<double> &y, char p) {
+	double r = 0, a = 0, xM = 0, yM = 0, xyM_sum = 0, xxM2_sum = 0, yyM2_sum = 0, Sx = 0, Sy = 0, b = 0;
 	std::vector<double> xxM, yyM, xyM, xxM2, yyM2;
 	xM = mean(x);
 	yM = mean(y);
+	long n = 0;
+
+	if (p == 'p') {
+		printf("( x )\t\t( y )\t\t( x - xM )\t( y - yM )\t(x-xM)*(y-yM)\t( (x-xM)^2 )\t( (y-yM)^2 )\n");
+	}
+
 	for (int i = 0; i < x.size(); i++) {
-		xxM[i] = x[i] - xM;
-		yyM[i] = y[i] - xM;
-		xyM[i] = xxM[i] * yyM[i];
-		xxM2[i] = pow(xxM[i], 2);
-		yyM2[i] = pow(yyM[i], 2);
+		xxM.push_back(x[i] - xM);
+		yyM.push_back(y[i] - yM);
+		xyM.push_back(xxM[i] * yyM[i]);
+		xxM2.push_back(pow(xxM[i], 2));
+		yyM2.push_back(pow(yyM[i], 2));
 		xyM_sum += xyM[i];
 		xxM2_sum += xxM2[i];
 		yyM2_sum += yyM2[i];
+		n += 1;
+		if (p == 'p') {
+			printf("[ %f ]\t[ %f ]\t[ %f ]\t[ %f ]\t[ %f ]\t[ %f ]\t[ %f ]\n", x[i], y[i], xxM[i], yyM[i], xyM[i], xxM2[i], yyM2[i]);
+		}
 	}
+	printf("xM = %f\tyM = %f\t\t\t\t\t\E = %f\tE = %f\tE = %f\n\n", xM, yM, xyM_sum, xxM2_sum, yyM2_sum);
+
+
 	r = xyM_sum / sqrt(xxM2_sum * yyM2_sum);
 
+	Sy = sqrt(yyM2_sum / (n - 1));
+	Sx = sqrt(xxM2_sum / (n - 1));
 
-	return r;
+	a = r * (Sy / Sx);
+
+	b = yM - a * xM;
+
+	
+
+	return {r, a, b};
 }
--- Linear Algebra Calculator/mathematics.cpp
+++ Linear Algebra Calculator/mathematics.cpp
@@ -18,6 +18,23 @@ bool checkFile(const char* name) {
     }
 }
 
+void clearCon() {
+    COORD topLeft = { 0, 0 };
+    HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO screen;
+    DWORD written;
+
+    GetConsoleScreenBufferInfo(console, &screen);
+    FillConsoleOutputCharacterA(
+        console, ' ', screen.dwSize.X * screen.dwSize.Y, topLeft, &written
+    );
+    FillConsoleOutputAttribute(
+        console, FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_BLUE,
+        screen.dwSize.X * screen.dwSize.Y, topLeft, &written
+    );
+    SetConsoleCursorPosition(console, topLeft);
+}
+
 int main()
 {
     std::fstream f;
@@ -30,64 +47,91 @@ int main()
         f.open("answers.txt", std::ios::out);
     }
 
+    Sleep(2000);
+    clearCon();
     std::string type;
-    
-    printf("\tplease enter what mode you want\n");
-    printf("\tget a & b with x & y in linear function (xy)\n");
-    printf("\tget intersection of 2 linear functions with a & b (inter)\n");
-    printf("\tget a in constant linear function from 1 point (const)\n");
-
-
     while (true) {
-        printf("\tq/quit to exit\n");
+        printf("\n\nplease enter what mode you want\n");
+        printf("\tget a & b with x & y in linear function (xy)\n");
+        printf("\tget intersection of 2 linear functions with a & b (inter)\n");
+        printf("\tget a in constant linear function from 1 point (const)\n");
+        printf("\tcalculate linear regression a, x & b from array (reg)\n");
+        printf("\tq / quit to exit\tc / clear to clear console\n");
+        
+
         type = "";
         std::cin >> type;
         
+        clearCon();
 
         if (type == "xy") {
-
             double x1, y1, x2, y2, a, b;
             printf("\t\tPlease enter 4 numbers, 2 x & 2 y values!\n");
 
+
             std::cin >> x1 >> y1 >> x2 >> y2;
             std::tie(a, b) = linearxy(x1, y1, x2, y2);
-            printf("\n");
-            std::cout << "x1: " << x1 << "\ty1: " << y1 << "\tx2: " << x2 << "\ty2: " << y2 << "\na: " << a << "\tb: " << b << "\n\n" << std::endl;
+
+            std::cout << "\nx1: " << x1 << "\ty1: " << y1 << "\tx2: " << x2 << "\ty2: " << y2 << "\na: " << a << "\tb: " << b << "\n\n" << std::endl;
             f << "x1: " << x1 << "\ty1: " << y1 << "\tx2: " << x2 << "\ty2: " << y2 << "\na: " << a << "\tb: " << b << "\n" << std::endl;
+            std::cin.clear();
         }
         else if (type == "inter") {
             double x, a1, b1, a2, b2;
-            std::cout << "\t\tplease enter 4 numbers 2 a & 2 b variables" << std::endl;
+            printf("\t\tplease enter 4 numbers 2 a & 2 b variables\n");
+
             std::cin >> a1 >> b1 >> a2 >> b2;
             x = intersection(a1, b1, a2, b2);
-            printf("\n");
-            std::cout << "a1: " << a1 << "\tb1: " << b1 << "\ta2: " << a2 << "\tb2: " << b2 << "\nx: " << x << "\n" << std::endl;
+
+            std::cout << "\na1: " << a1 << "\tb1: " << b1 << "\ta2: " << a2 << "\tb2: " << b2 << "\nx: " << x << "\n" << std::endl;
             f << "a1: " << a1 << "\tb1: " << b1 << "\ta2: " << a2 << "\tb2: " << b2 << "\nx: " << x << "\n" << std::endl;
+            std::cin.clear();
         }
         else if (type == "const") {
             double x, y, k;
             printf("\t\tplease enter 2 numbers, x & y\n");
+
             std::cin >> x >> y;
             k = constant(x, y);
-            printf("\nx: %f\ty: %f\nconstant: %f", x, y, k);
+
+            printf("\nx: %f\ty: %f\nconstant: %f\n", x, y, k);
             f << "x: " << x << "\ty: " << y << "\nconstant: " << k << "\n" << std::endl;
+            std::cin.clear();
         }
         else if (type == "reg") {
             std::vector<double> x, y;
-            double r;
-            printf("enter x values (non-numbers to continue)\n");
+            double r, a, b;
+            char p; std::string pTemp;
+            printf("enter x values (non-numbers to continue / p to print)\n");
             x = fillVector(x);
-            printf("enter y values (non-numbers to continue)\n");
-            y = fillVector(y);
+            std::cin >> pTemp;
+            if (pTemp[0] == 'p') {
+                p = 'p';
+            }
+            else {
+                p = 'x';
+            }
+            std::cin.clear();
+            printf("enter y values\n");
+            y = fillVector(y, x.size());
+            
+            std::tie(r, a, b) = regression(x, y, p);
 
-            r = pearson(x, y);
-            printf("r: %f\n", r);
+            printf("r: %f\ta: %f\tb: %f\n", r, a, b);
+            f << "r: " << r << "\na: " << a << "\nb: " << b << "\n" << std::endl;
+            std::cin.clear();
         }
-        else if (type == "q" || type == "quit") {
+        else if (type[0] == 'q') {
             f.close();
+            std::cin.clear();
+            clearCon();
             return 0;
         }
+        else if (type[0] == 'c') {
+            clearCon();
+        }
         else {
+            std::cin.clear();
             printf("\nplease enter a valid mode\n\n");
         }
     }
